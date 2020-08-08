a24874b8caa35c4d80eff116222ece8e7224e9be
martin <martdevelopers254@gmail.com>
2020-08-08 16:56:10 +0300

    Finalized off canvas menu

--- resources/views/partials/off_canvas.blade.php
+++ resources/views/partials/off_canvas.blade.php
@@ -17,191 +17,41 @@
         </div>
     </div>
     <div class="offcanvas-scrollable-area border-top" style="height:calc(100% - 235px); top: 144px;">
-    <!-- Mobile Menu-->
-    <div class="accordion mobile-menu" id="accordion-menu">
-        <!-- Home-->
-        <div class="card">
-        <div class="card-header"><a class="mobile-menu-link active" href="index-2.html">Home</a><a class="collapsed" href="#home-submenu" data-toggle="collapse"></a></div>
-        <div class="collapse" id="home-submenu" data-parent="#accordion-menu">
-            <div class="card-body">
-            <ul>
-                <li class="dropdown-header"><i class="fe-icon-briefcase"></i>&nbsp;&nbsp;Portfolio</li>
-                <li class="dropdown-item"><a href="home-digital-agency.html">Digital Agency</a></li>
-                <li class="dropdown-item"><a class="disabled-link" href="#">Architectural Agency<span class="badge badge-secondary">Soon</span></a></li>
-                <li class="dropdown-item"><a href="home-freelancer-portfolio.html">Freelancer's Portfolio</a></li>
-                <li class="dropdown-item"><a href="home-photographer-portfolio.html">Photographer's Portfolio</a></li>
-                <li class="dropdown-header"><i class="fe-icon-shopping-bag"></i>&nbsp;&nbsp;E-Commerce</li>
-                <li class="dropdown-item"><a href="home-shop-v1.html">Online Shop v.1</a></li>
-                <li class="dropdown-item"><a href="home-shop-v2.html">Online Shop v.2</a></li>
-                <li class="dropdown-header"><i class="fe-icon-edit"></i>&nbsp;&nbsp;Blog</li>
-                <li class="dropdown-item"><a href="home-corporate-blog.html">Corporate Blog</a></li>
-                <li class="dropdown-item"><a href="home-personal-blog.html">Personal Blog</a></li>
-                <li class="dropdown-header"><i class="fe-icon-home"></i>&nbsp;&nbsp;Landing Pages</li>
-                <li class="dropdown-item"><a href="home-mobile-app-showcase.html">Mobile App Showcase</a></li>
-                <li class="dropdown-item"><a href="home-web-app-showcase.html">Web App Showcase (SAAS)</a></li>
-                <li class="dropdown-item"><a class="disabled-link" href="#">Product Promo Page<span class="badge badge-secondary">Soon</span></a></li>
-                <li class="dropdown-item"><a href="home-conference-landing.html">Conference Landing</a></li>
-                <li class="dropdown-header"><i class="fe-icon-dollar-sign"></i>&nbsp;&nbsp;Business</li>
-                <li class="dropdown-item"><a class="disabled-link" href="#">Corporate Website<span class="badge badge-secondary">Soon</span></a></li>
-                <li class="dropdown-item"><a href="home-marketing-seo.html">Digital Marketing &amp; SEO</a></li>
-                <li class="dropdown-item"><a href="home-capital-investments.html">Venture Capital Investments</a></li>
-                <li class="dropdown-item"><a class="disabled-link" href="#">Startup Homepage<span class="badge badge-secondary">Soon</span></a></li>
-                <li class="dropdown-header"><i class="fe-icon-menu"></i>&nbsp;&nbsp;Directory &amp; Listings</li>
-                <li class="dropdown-item"><a class="disabled-link" href="#">Real Estate Listing<span class="badge badge-secondary">Soon</span></a></li>
-                <li class="dropdown-item"><a class="disabled-link" href="#">Jobs Board<span class="badge badge-secondary">Soon</span></a></li>
-            </ul>
+        <!-- Mobile Menu-->
+        <div class="accordion mobile-menu" id="accordion-menu">
+            <!-- Home-->
+            <div class="card">
+                <div class="card-header">
+                    <a class="mobile-menu-link" href="{{ url('/') }}">Home</a>
+                </div>
             </div>
-        </div>
-        </div>
-        <!-- Portfolio-->
-        <div class="card">
-        <div class="card-header"><a class="mobile-menu-link" href="portfolio-style1-boxed.html">Portfolio</a><a class="collapsed" href="#portfolio-submenu" data-toggle="collapse"></a></div>
-        <div class="collapse" id="portfolio-submenu" data-parent="#accordion-menu">
-            <div class="card-body">
-            <ul>
-                <li class="dropdown-item"><a href="portfolio-style1-boxed.html">Style 1 Grid</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="portfolio-style1-boxed.html">Style 1 Boxed Layout</a></li>
-                    <li class="dropdown-item"><a href="portfolio-style1-fw.html">Style 1 Full Width Layout</a></li>
-                </ul>
-                </li>
-                <li class="dropdown-item"><a href="portfolio-style2-boxed.html">Style 2 Grid</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="portfolio-style2-boxed.html">Style 2 Boxed Layout</a></li>
-                    <li class="dropdown-item"><a href="portfolio-style2-fw.html">Style 2 Full Width Layout</a></li>
-                </ul>
-                </li>
-                <li class="dropdown-item"><a href="portfolio-single-side-gallery-grid.html">Single Project</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="portfolio-single-side-gallery-grid.html">Side Gallery Grid</a></li>
-                    <li class="dropdown-item"><a href="portfolio-single-side-gallery-list.html">Side Gallery List</a></li>
-                    <li class="dropdown-item"><a href="portfolio-single-wide-slider.html">Wide Slider</a></li>
-                    <li class="dropdown-item"><a href="portfolio-single-wide-gallery.html">Wide Gallery</a></li>
-                </ul>
-                </li>
-            </ul>
+
+            <!-- About UniSys-->
+            <div class="card">
+                <div class="card-header">
+                    <a class="mobile-menu-link" href="{{ url('/about') }}">
+                        About UniSys
+                    </a>
+                </div>
             </div>
-        </div>
-        </div>
-        <!-- Blog-->
-        <div class="card">
-        <div class="card-header"><a class="mobile-menu-link" href="blog-grid-rs.html">Blog</a><a class="collapsed" href="#blog-submenu" data-toggle="collapse"></a></div>
-        <div class="collapse" id="blog-submenu" data-parent="#accordion-menu">
-            <div class="card-body">
-            <ul>
-                <li class="dropdown-header">Blog Layouts</li>
-                <li class="dropdown-item"><a href="blog-grid-rs.html">Grid Right Sidebar</a></li>
-                <li class="dropdown-item"><a href="blog-grid-ls.html">Grid Left Sidebar</a></li>
-                <li class="dropdown-item"><a href="blog-grid-ns.html">Grid No Sidebar</a></li>
-                <li class="dropdown-header">Single Post Formats</li>
-                <li class="dropdown-item"><a href="blog-single-gallery.html">Post Gallery</a></li>
-                <li class="dropdown-item"><a href="blog-single-carousel.html">Post Image Carousel</a></li>
-                <li class="dropdown-item"><a href="blog-single-video.html">Post Video</a></li>
-                <li class="dropdown-item"><a href="blog-single-audio.html">Post Audio</a></li>
-                <li class="dropdown-item"><a href="blog-single-quotation.html">Post Quotation</a></li>
-                <li class="dropdown-item"><a href="blog-single-link.html">Post Link</a></li>
-            </ul>
+
+            <!--Features-->
+            <div class="card">
+                <div class="card-header">
+                    <a class="mobile-menu-link" href="{{ url('/features') }}">
+                        UniSys Features
+                    </a>
+                </div>
             </div>
-        </div>
-        </div>
-        <!-- Shop-->
-        <div class="card">
-        <div class="card-header"><a class="mobile-menu-link" href="shop-boxed-ls.html">Shop</a><a class="collapsed" href="#shop-submenu" data-toggle="collapse"></a></div>
-        <div class="collapse" id="shop-submenu" data-parent="#accordion-menu">
-            <div class="card-body">
-            <ul>
-                <li class="dropdown-header">Shop Layouts</li>
-                <li class="dropdown-item"><a href="shop-boxed-ls.html">Boxed Left Sidebar</a></li>
-                <li class="dropdown-item"><a href="shop-boxed-rs.html">Boxed Right Sidebar</a></li>
-                <li class="dropdown-item"><a href="shop-boxed-ft.html">Boxed Filters Top</a></li>
-                <li class="dropdown-item"><a href="shop-boxed-ns.html">Boxed No Sidebar</a></li>
-                <li class="dropdown-item"><a href="shop-fw-ls.html">Full Width Left Sidebar</a></li>
-                <li class="dropdown-item"><a href="shop-fw-rs.html">Full Width Right Sidebar</a></li>
-                <li class="dropdown-item"><a href="shop-fw-ft.html">Full Width Filters Top</a></li>
-                <li class="dropdown-item"><a href="shop-fw-ns.html">Full Width No Sidebar</a></li>
-                <li class="dropdown-header">Shop Pages</li>
-                <li class="dropdown-item"><a href="shop-categories.html">Shop Categories</a></li>
-                <li class="dropdown-item"><a href="shop-single.html">Product Page</a></li>
-                <li class="dropdown-item"><a href="cart.html">Cart</a></li>
-                <li class="dropdown-item"><a href="checkout-address.html">Checkout - Address</a></li>
-                <li class="dropdown-item"><a href="checkout-shipping.html">Checkout - Shipping</a></li>
-                <li class="dropdown-item"><a href="checkout-payment.html">Checkout - Payment</a></li>
-                <li class="dropdown-item"><a href="checkout-review.html">Checkout - Review</a></li>
-                <li class="dropdown-item"><a href="checkout-complete.html">Checkout - Complete</a></li>
-            </ul>
+
+            <!--Installation Steps-->
+            <div class="card">
+                <div class="card-header">
+                    <a class="mobile-menu-link" href="{{ url('/user-manual') }}">
+                        User Manual
+                    </a>
+                </div>
             </div>
-        </div>
-        </div>
-        <!-- Account-->
-        <div class="card">
-        <div class="card-header"><a class="mobile-menu-link" href="account-orders.html">Account</a><a class="collapsed" href="#account-submenu" data-toggle="collapse"></a></div>
-        <div class="collapse" id="account-submenu" data-parent="#accordion-menu">
-            <div class="card-body">
-            <ul>
-                <li class="dropdown-item"><a href="account-login.html">Login / Register</a></li>
-                <li class="dropdown-item"><a href="account-password-recovery.html">Password Recovery</a></li>
-                <li class="dropdown-item"><a href="account-orders.html">Orders List</a></li>
-                <li class="dropdown-item"><a href="account-profile.html">Profile Settings</a></li>
-                <li class="dropdown-item"><a href="account-address.html">Contact / Shipping Address</a></li>
-                <li class="dropdown-item"><a href="account-wishlist.html">Wishlist</a></li>
-                <li class="dropdown-item"><a href="account-tickets.html">My Tickets</a></li>
-                <li class="dropdown-item"><a href="account-single-ticket.html">Single Ticket</a></li>
-            </ul>
-            </div>
-        </div>
-        </div>
-        <!-- Pages-->
-        <div class="card">
-        <div class="card-header"><a class="mobile-menu-link" href="#">Pages</a><a class="collapsed" href="#pages-submenu" data-toggle="collapse"></a></div>
-        <div class="collapse" id="pages-submenu" data-parent="#accordion-menu">
-            <div class="card-body">
-            <ul>
-                <li class="dropdown-item"><a href="about-us-v1.html">About Us</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="about-us-v1.html">About Us v.1</a></li>
-                    <li class="dropdown-item"><a href="about-us-v2.html">About Us v.2</a></li>
-                </ul>
-                </li>
-                <li class="dropdown-item"><a href="contacts-v1.html">Contacts</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="contacts-v1.html">Contacts v.1</a></li>
-                    <li class="dropdown-item"><a href="contacts-v2.html">Contacts v.2</a></li>
-                </ul>
-                </li>
-                <li class="dropdown-item"><a href="help-topics.html">Help Center</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="help-topics.html">Help Topics</a></li>
-                    <li class="dropdown-item"><a href="help-single-topic.html">Single Topic</a></li>
-                    <li class="dropdown-item"><a href="help-submit-request.html">Submit a Request</a></li>
-                </ul>
-                </li>
-                <li class="dropdown-item"><a href="services.html">Services</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="services.html">Services Landing</a></li>
-                    <li class="dropdown-item"><a href="services-single.html">Single Service Page</a></li>
-                </ul>
-                </li>
-                <li class="dropdown-item"><a href="pricing.html">Pricing</a></li>
-                <li class="dropdown-item"><a href="product-comparison.html">Product Comparison</a></li>
-                <li class="dropdown-item"><a href="order-tracking.html">Order Tracking</a></li>
-                <li class="dropdown-item"><a href="search-results.html">Search Results</a></li>
-                <li class="dropdown-item"><a href="coming-soon-light.html">Coming Soon</a>
-                <ul class="mobile-sublevel">
-                    <li class="dropdown-item"><a href="coming-soon-light.html">Coming Soon Light</a></li>
-                    <li class="dropdown-item"><a href="coming-soon-dark.html">Coming Soon Dark</a></li>
-                </ul>
-                </li>
-                <li class="dropdown-item"><a href="404.html">404 Not Found</a></li>
-                <li class="dropdown-item"><a class="text-accent" href="docs/dev-setup.html"><i class="d-inline-block fe-icon-file-text align-middle mr-1"></i>Documentation</a></li>
-            </ul>
-            </div>
-        </div>
-        </div>
-        <!-- Components-->
-        <div class="card">
-        <div class="card-header"><a class="mobile-menu-link" href="components.html">Components</a></div>
         </div>
     </div>
-    </div>
-    <div class="offcanvas-footer px-4 pt-3 pb-2 text-center"><a class="social-btn sb-style-3 sb-twitter" href="#"><i class="socicon-twitter"></i></a><a class="social-btn sb-style-3 sb-facebook" href="#"><i class="socicon-facebook"></i></a><a class="social-btn sb-style-3 sb-pinterest" href="#"><i class="socicon-pinterest"></i></a><a class="social-btn sb-style-3 sb-instagram" href="#"><i class="socicon-instagram"></i></a></div>
 </div>

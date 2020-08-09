df09ce17f105d97a32d20fd17830e4f34688c5ea
MartMbithi <martdevelopers254@gmail.com>
2020-08-09 15:26:42 +0300

    Implemented About page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>UniSys - Light Weight University Information Management Syste</title>
-    <!-- SEO Meta Tags-->
-    <meta name="description" content="UniSys">
-    <meta name="keywords" content="multipurpose, light weight, information management system,responsive,  University, bootstrap 4, html5, css3, jquery, js,clean">
-    <meta name="author" content="MartDevelopers Inc , Team Devlan">
-    <!-- Viewport-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Favicon and Touch Icons-->
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="mask-icon" color="#343b43" href="safari-pinned-tab.svg">
-    <meta name="msapplication-TileColor" content="#603cba">
-    <meta name="theme-color" content="#ffffff">
-    <!-- Vendor Styles including: Font Icons, Plugins, etc.-->
-    <link rel="stylesheet" media="screen" href="{{ url('css/vendor.min.css') }}">
-    <!-- Main Theme Styles + Bootstrap-->
-    <link rel="stylesheet" media="screen" href="{{ url('css/theme.min.css') }}">
-    <!-- Modernizr-->
-    <script src="{{ url('js/modernizr.min.js') }}"></script>
-  </head>
+  @include('partials.head')
   <!-- Body-->
   <body>
     <!-- Off-Canvas Menu-->

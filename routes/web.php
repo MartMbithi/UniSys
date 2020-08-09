4d5d06a0aabaf571f5d13b1de2b7b400a542efb6
MartMbithi <martdevelopers254@gmail.com>
2020-08-09 15:20:33 +0300

    Added About Route

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+    //home page route
+});
+
+Route::get('/about', function ()
+{
+    return view('about');
+    //about page route
 });

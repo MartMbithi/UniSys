4004ded88d796bd2d842679ee22160d7e6a5af32
MartMbithi <martdevelopers254@gmail.com>
2020-08-10 16:36:05 +0300

    Updated Documentation

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -21,39 +21,25 @@
     </div>
     <!-- Page Content-->
     <div class="container pb-5 mb-3">
-      <!-- Buy Online-->
       <div class="d-md-table w-100 p-4 p-lg-5 mb-30 box-shadow">
-        <div class="d-md-table-cell align-middle mx-auto mb-4 mb-md-0" style="width: 150px;"><img class="d-block w-100" src="img/pages/about-icons/01.png" alt="Buy Online"></div>
+        <div class="d-md-table-cell align-middle mx-auto mb-4 mb-md-0" style="width: 150px;">
+        <img class="d-block w-100" src="{{ url('img/pages/about-icons/02.png') }}" alt="Buy Online"></div>
         <div class="d-md-table-cell align-middle pl-md-4 pl-lg-5 text-center text-md-left">
-          <h3 class="h4">Search, Select, Buy Online.</h3>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor, tristique nec placerat nec.</p><a class="font-weight-medium text-decoration-none" href="shop-boxed-ls.html">View Products<i class="fe-icon-arrow-right d-inline-block align-middle ml-1"></i></a>
+          <h3 class="h4">Required Packages</h3>
+          <p class="text-muted">
+          </p>
         </div>
       </div>
-      <!-- Delivery Worldwide-->
       <div class="d-md-table w-100 p-4 p-lg-5 mb-30 box-shadow">
-        <div class="d-md-table-cell align-middle mx-auto mb-4 mb-md-0" style="width: 150px;"><img class="d-block w-100" src="img/pages/about-icons/02.png" alt="Delivery Worldwide"></div>
+        <div class="d-md-table-cell align-middle mx-auto mb-4 mb-md-0" style="width: 150px;">
+        <img class="d-block w-100" src="{{ url('img/pages/about-icons/01.png') }}" alt="Delivery Worldwide"></div>
         <div class="d-md-table-cell align-middle pl-md-4 pl-lg-5 text-center text-md-left">
-          <h3 class="h4">Fast Delivery Worldwide.</h3>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor, tristique nec placerat nec.</p><a class="font-weight-medium text-decoration-none" href="#">Shipping Details<i class="fe-icon-arrow-right d-inline-block align-middle ml-1"></i></a>
+          <h3 class="h4">How To Install</h3>
+          <p class="text-muted">
+
+          </p>
         </div>
       </div>
-      <!-- Mobile App-->
-      <div class="d-md-table w-100 p-4 p-lg-5 mb-30 box-shadow">
-        <div class="d-md-table-cell align-middle mx-auto mb-4 mb-md-0" style="width: 150px;"><img class="d-block w-100" src="img/pages/about-icons/03.png" alt="Mobile App"></div>
-        <div class="d-md-table-cell align-middle pl-md-4 pl-lg-5 text-center text-md-left">
-          <h3 class="h4">Great Mobile App. Shop On The Go.</h3>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat.</p><a class="market-btn apple-btn mr-3 mt-3" href="#"><span class="mb-subtitle">Download on the</span><span class="mb-title">App Store</span></a><a class="market-btn google-btn mr-3 mt-3" href="#"><span class="mb-subtitle">Download on the</span><span class="mb-title">Google Play</span></a><a class="market-btn windows-btn mr-3 mt-3" href="#"><span class="mb-subtitle">Download on the</span><span class="mb-title">Windows Store</span></a>
-        </div>
-      </div>
-      <!-- Outlet Stores-->
-      <div class="d-md-table w-100 p-4 p-lg-5 box-shadow">
-        <div class="d-md-table-cell align-middle mx-auto mb-4 mb-md-0" style="width: 150px;"><img class="d-block w-100" src="img/pages/about-icons/04.png" alt="Outlet Stores"></div>
-        <div class="d-md-table-cell align-middle pl-md-4 pl-lg-5 text-center text-md-left">
-          <h3 class="h4">Shop Offline. Cozy Outlet Stores.</h3>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor, tristique nec placerat nec.</p><a class="font-weight-medium text-decoration-none" href="contacts-v2.html">See Outlet Stores<i class="fe-icon-arrow-right d-inline-block align-middle ml-1"></i></a>
-        </div>
-      </div>
-           
     </div>
     <!-- Footer-->
     @include('partials.footer')

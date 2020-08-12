862b1e34ea55132a2d62dc17ec75045b6e49f9c9
MartMbithi <martdevelopers254@gmail.com>
2020-08-12 13:59:53 +0300

    Updated Modules-br

--- resources/views/course-module.blade.php
+++ resources/views/course-module.blade.php
@@ -27,8 +27,8 @@
         <div class="d-md-table-cell align-middle pl-md-4 pl-lg-5 text-center text-md-left">
           <h3 class="h4">Course And Programe Information Management Module</h3>
           <p class="text-muted">
-            <li>Automatic enrollment: Logic within an LMS which registers and reminds employees about mandatory courses.</li>
-            <li>Enhanced Security: Many corporate LMS solutions have single sign in, advanced authentication, and firewalls to ensure data security.</li>
+            <li>Automatic enrollment: Logic within a Unisys, which registers and reminds students about mandatory courses.</li>
+            <li>Enhanced Security: UniSys have single sign in, advanced authentication, and firewalls to ensure data security.</li>
             <li>A digital roll-call sheet for tracking attendance and for sending invitations to class participants.</li>
             <li>Distributed instructor and student base: Remote participation by the instructor or pupil allows courseware to feature multiple teachers or experts from across the globe.</li>
             <li>Course calendars: Creation and publication of important dates related to the course schedules, including project deadlines and tests.</li>
@@ -36,16 +36,15 @@
             <li>Assessment and testing: Creation of knowledge retention exercises such as short quizzes and comprehensive exams.</li>
             <li>Grading and Scoring: Advanced tracking and charting of student performance over time. </li>
 
-            <b><li>Advanced LMS features</li></b>
+            <b><li>Advanced Unisys - Course And Programme Information Management Module features</li></b>
 
-            <p>In addition to these basic features, there are many advanced features an LMS may include, such as:</p>
+            <p>In addition to these basic features, there are many advanced features, such as:</p>
 
-            <li>eCommerce: The ability to sell courses to third parties, and integrate with payment processors such as PayPal and Stripe.</li>
             <li>eConferencing: The ability to organize and hold e-conference sessions, with multiple students participating through audio and video.</li>
             <li>Excel Uploader: Using MS Excel, administrators can easily create multiple users accounts, upload hundreds of classroom and training records, and download training reports within minutes.</li>
             <li>Whiteboard: Online whiteboard functionality, so instructors and students can create and share writings and drawings in real time.</li>
-            <li>Mobile friendly: The ability to use the LMS with mobile devices (smartphones and tablets), including being able to study when offline.</li>
-            <li>Custom branding: Many LMS systems have the ability to use a company’s own branding and/or can create custom themes for the LMS user interface.</li>        
+            <li>Mobile friendly: The ability to use the UniSys with mobile devices (smartphones and tablets), including being able to study when offline.</li>
+            <li>Custom branding: Unisys has  the ability to use a company’s own branding and/or can create custom themes for the UniSys user interface.</li>        
           </p>
         </div>
       </div>

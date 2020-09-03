86299da8950c4a30340ba0d24671d85593954f9a
MartMbithi <martdevelopers254@gmail.com>
2020-09-03 16:33:16 +0300

    Cranked Auth Footer

---
+++ Modules/SRM/partials/_scripts.php
@@ -0,0 +1,48 @@
+<!-- jquery
+		============================================ -->
+<script src="js/vendor/jquery-1.12.4.min.js"></script>
+<!-- bootstrap JS
+		============================================ -->
+<script src="js/bootstrap.min.js"></script>
+<!-- wow JS
+		============================================ -->
+<script src="js/wow.min.js"></script>
+<!-- price-slider JS
+		============================================ -->
+<script src="js/jquery-price-slider.js"></script>
+<!-- meanmenu JS
+		============================================ -->
+<script src="js/jquery.meanmenu.js"></script>
+<!-- owl.carousel JS
+		============================================ -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- sticky JS
+		============================================ -->
+<script src="js/jquery.sticky.js"></script>
+<!-- scrollUp JS
+		============================================ -->
+<script src="js/jquery.scrollUp.min.js"></script>
+<!-- mCustomScrollbar JS
+		============================================ -->
+<script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/scrollbar/mCustomScrollbar-active.js"></script>
+<!-- metisMenu JS
+		============================================ -->
+<script src="js/metisMenu/metisMenu.min.js"></script>
+<script src="js/metisMenu/metisMenu-active.js"></script>
+<!-- tab JS
+		============================================ -->
+<script src="js/tab.js"></script>
+<!-- icheck JS
+		============================================ -->
+<script src="js/icheck/icheck.min.js"></script>
+<script src="js/icheck/icheck-active.js"></script>
+<!-- plugins JS
+		============================================ -->
+<script src="js/plugins.js"></script>
+<!-- main JS
+		============================================ -->
+<script src="js/main.js"></script>
+<!-- tawk chat JS
+		============================================ -->
+<script src="js/tawk-chat.js"></script>

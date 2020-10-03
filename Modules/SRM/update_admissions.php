0fb9ac6277af28d1a3c2a72828a92821c1c9d25a
MartMbithi <martdevelopers254@gmail.com>
2020-10-03 18:56:47 +0300

    Update Functionality On Admissions

--- Modules/SRM/update_admissions.php
+++ Modules/SRM/update_admissions.php
@@ -46,102 +46,110 @@ require_once('partials/_head.php');
     <!-- Start Welcome area -->
     <div class="all-content-wrapper">
         <br><br><br><br><br>
-        <?php require_once('partials/_header.php'); ?>
-        <!-- Single pro tab review Start-->
-        <div class="single-pro-review-area mt-t-30 mg-b-15">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="product-payment-inner-st">
-                            <ul id="myTabedu1" class="tab-review-design">
-                                <li class="active"><a href="#description">Add New Student Admission Record</a></li>
-                            </ul>
-                            <div id="myTabContent" class="tab-content custom-product-edit">
-                                <div class="product-tab-list tab-pane fade active in" id="description">
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                            <div class="review-content-section">
-                                                <form method="POST" id="add-department" action="#" class="add-department">
-                                                    <div class="row">
-                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                                                            <div class="form-group">
-                                                                <input name="name" type="text" class="form-control" placeholder="Student Name">
-                                                                <input name="id" value="<?php echo $facultyID; ?>" type="hidden" class="form-control" placeholder="Name">
+        <?php
+        require_once('partials/_header.php');
+        $update = $_GET['update'];
+        $ret = "SELECT * FROM `UniSys_Students` WHERE id ='$update'  ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($std = $res->fetch_object()) {
+        ?>
+            <!-- Single pro tab review Start-->
+            <div class="single-pro-review-area mt-t-30 mg-b-15">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="product-payment-inner-st">
+                                <ul id="myTabedu1" class="tab-review-design">
+                                    <li class="active"><a href="#description">Update <?php echo $std->name; ?> Admission Record</a></li>
+                                </ul>
+                                <div id="myTabContent" class="tab-content custom-product-edit">
+                                    <div class="product-tab-list tab-pane fade active in" id="description">
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                <div class="review-content-section">
+                                                    <form method="POST" id="add-department" action="#" class="add-department">
+                                                        <div class="row">
+                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                                                <div class="form-group">
+                                                                    <input name="name" value="<?php echo $std->name; ?>" type="text" class="form-control" placeholder="Student Name">
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <input required name="reg_no" value="<?php echo $std->reg_no; ?>" type="text" class="form-control" placeholder="Registration Number">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                                                <div class="form-group">
+                                                                    <input required name="campus_email" value="<?php echo $std->campus_email; ?>" type="text" class="form-control" placeholder="Campus Email">
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <input required name="personal_email" value="<?php echo $std->personal_email; ?>" type="text" class="form-control" placeholder="Personal Email">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                                                <div class="form-group">
+                                                                    <input required name="idnumber" value="<?php echo $std->idnumber; ?>" type="text" class="form-control" placeholder="National ID Number">
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <input required name="phone" value="<?php echo $std->phone; ?>" type="text" class="form-control" placeholder="Phone Number">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-4">
+                                                                <div class="form-group">
+                                                                    <select name="gender" class="form-control">
+                                                                        <option><?php echo $std->gender; ?></option>
+                                                                        <option>Male</option>
+                                                                        <option>Female</option>
+                                                                    </select>
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <input required name="dob" value="<?php echo $std->dob; ?>" type="text" class="form-control" placeholder="Date Of Birth">
+                                                                </div>
+
                                                             </div>
 
-                                                            <div class="form-group">
-                                                                <input required name="reg_no" type="text" class="form-control" placeholder="Registration Number">
+                                                            <div class=" form-row col-lg-12 col-md-12 col-sm-12 col-xs-6">
+                                                                <div class="form-group">
+                                                                    <input required name="adr" value="<?php echo $std->adr; ?>" type="text" class="form-control" placeholder="Address">
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <input required name="country" value="<?php echo $std->country; ?>" type="text" class="form-control" placeholder="Country">
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <select name="course_name" id="courseName" onchange="getCourseDetails(this.value);" class="form-control">
+                                                                        <option>Select Course</option>
+                                                                        <?php
+                                                                        $ret = "SELECT * FROM `UniSys_Courses`  ";
+                                                                        $stmt = $mysqli->prepare($ret);
+                                                                        $stmt->execute(); //ok
+                                                                        $res = $stmt->get_result();
+                                                                        while ($course = $res->fetch_object()) {
+                                                                        ?>
+                                                                            <option><?php echo $course->course_name; ?></option>
+                                                                        <?php
+                                                                        } ?>
+                                                                    </select>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <input name="course_id" id="Course_Id" type="hidden" class="form-control" placeholder="Course Id">
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                                                            <div class="form-group">
-                                                                <input required name="campus_email" type="text" class="form-control" placeholder="Campus Email">
-                                                            </div>
-
-                                                            <div class="form-group">
-                                                                <input required name="personal_email" type="text" class="form-control" placeholder="Personal Email">
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                                                            <div class="form-group">
-                                                                <input required name="idnumber" type="text" class="form-control" placeholder="National ID Number">
-                                                            </div>
-
-                                                            <div class="form-group">
-                                                                <input required name="phone" type="text" class="form-control" placeholder="Phone Number">
+                                                        <br>
+                                                        <div class="row">
+                                                            <div class="col-lg-12">
+                                                                <div class="payment-adress">
+                                                                    <button type="submit" name="update" class="btn btn-primary waves-effect waves-light">Submit</button>
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-4">
-                                                            <div class="form-group">
-                                                                <select name="gender" class="form-control">
-                                                                    <option>Select Gender</option>
-                                                                    <option>Male</option>
-                                                                    <option>Female</option>
-                                                                </select>
-                                                            </div>
-
-                                                            <div class="form-group">
-                                                                <input required name="dob" type="text" class="form-control" placeholder="Date Of Birth">
-                                                            </div>
-
-                                                        </div>
-
-                                                        <div class=" form-row col-lg-12 col-md-12 col-sm-12 col-xs-6">
-                                                            <div class="form-group">
-                                                                <input required name="adr" type="text" class="form-control" placeholder="Address">
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <input required name="country" type="text" class="form-control" placeholder="Country">
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <select name="course_name" id="courseName" onchange="getCourseDetails(this.value);" class="form-control">
-                                                                    <option>Select Course</option>
-                                                                    <?php
-                                                                    $ret = "SELECT * FROM `UniSys_Courses`  ";
-                                                                    $stmt = $mysqli->prepare($ret);
-                                                                    $stmt->execute(); //ok
-                                                                    $res = $stmt->get_result();
-                                                                    while ($course = $res->fetch_object()) {
-                                                                    ?>
-                                                                        <option><?php echo $course->course_name; ?></option>
-                                                                    <?php
-                                                                    } ?>
-                                                                </select>
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <input name="course_id" id="Course_Id" type="hidden" class="form-control" placeholder="Course Id">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <br>
-                                                    <div class="row">
-                                                        <div class="col-lg-12">
-                                                            <div class="payment-adress">
-                                                                <button type="submit" name="add" class="btn btn-primary waves-effect waves-light">Submit</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </form>
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -151,9 +159,9 @@ require_once('partials/_head.php');
                     </div>
                 </div>
             </div>
-        </div>
-        <br><br><br>
-        <?php require_once('partials/_footer.php'); ?>
+            <br><br><br>
+        <?php require_once('partials/_footer.php');
+        } ?>
     </div>
     <?php require_once('partials/_scripts.php'); ?>
 </body>

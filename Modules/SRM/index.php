eca160ffee06b3481ac36bd6d265f87002d1444f
MartMbithi <martdevelopers254@gmail.com>
2020-10-07 11:03:32 +0300

    Ui on Login Update

--- Modules/SRM/index.php
+++ Modules/SRM/index.php
@@ -26,7 +26,8 @@ require_once('partials/_head.php');
             <div class="form-form-wrap">
                 <div class="form-container">
                     <div class="form-content">
-                        <h1 class="">Sign In</h1>
+                        <img class="img-fluid text-center" src="assets/img/logo.png">
+                        <h1>Students' Records Management Module</h1>
                         <form method="post" class="text-left">
                             <div class="form">
                                 <div id="username-field" class="field-wrapper input">
@@ -35,7 +36,7 @@ require_once('partials/_head.php');
                                         <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                         <circle cx="12" cy="7" r="4"></circle>
                                     </svg>
-                                    <input id="username" name="auth_email" type="text" class="form-control" placeholder="e.g John_Doe">
+                                    <input id="username" name="auth_email" type="text" class="form-control">
                                 </div>
                                 <div id="password-field" class="field-wrapper input mb-2">
                                     <div class="d-flex justify-content-between">
@@ -46,7 +47,7 @@ require_once('partials/_head.php');
                                         <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                                         <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
                                     </svg>
-                                    <input id="password" name="auth_password" type="password" class="form-control" placeholder="Password">
+                                    <input id="password" name="auth_password" type="password" class="form-control">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye">
                                         <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                         <circle cx="12" cy="12" r="3"></circle>

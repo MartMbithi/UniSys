cd47d5e097076d20102701060ca03608afd8c2a9
MartMbithi <martdevelopers254@gmail.com>
2020-10-07 10:50:43 +0300

    Configured Configs / Loaded Partials

---
+++ Modules/SRM/partials/_auth-footer.php
@@ -0,0 +1,3 @@
+<div class="text-center login-footer">
+    <p>Copyright © 2020 - <?php echo date('Y');?>. All rights reserved. Powered By <a href="https://martdev.info/">MartDevelopers Inc</a></p>
+</div>

c0afb1d36709cb72f4b75e19cfdbe7805bcbdec5
MartMbithi <martdevelopers254@gmail.com>
2020-10-08 15:01:43 +0300

    UI Updates On Admissions

--- Modules/SRM/unisys_srm_view_admission.php
+++ Modules/SRM/unisys_srm_view_admission.php
@@ -98,91 +98,39 @@ require_once('partials/_head.php');
                                         <div class="">
                                             <ul class="contacts-block list-unstyled">
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-coffee">
-                                                        <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
-                                                        <path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
-                                                        <line x1="6" y1="1" x2="6" y2="4"></line>
-                                                        <line x1="10" y1="1" x2="10" y2="4"></line>
-                                                        <line x1="14" y1="1" x2="14" y2="4"></line>
-                                                    </svg> ADM No: <?php echo $std->name; ?>
+                                                    ADM No: <?php echo $std->name; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                                        <line x1="16" y1="2" x2="16" y2="6"></line>
-                                                        <line x1="8" y1="2" x2="8" y2="6"></line>
-                                                        <line x1="3" y1="10" x2="21" y2="10"></line>
-                                                    </svg> Campus Email : <?php echo $std->campus_email; ?>
+                                                    Campus Email : <?php echo $std->campus_email; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                                        <line x1="16" y1="2" x2="16" y2="6"></line>
-                                                        <line x1="8" y1="2" x2="8" y2="6"></line>
-                                                        <line x1="3" y1="10" x2="21" y2="10"></line>
-                                                    </svg> Personal Email : <?php echo $std->personal_email; ?>
+                                                    Personal Email : <?php echo $std->personal_email; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                                        <line x1="16" y1="2" x2="16" y2="6"></line>
-                                                        <line x1="8" y1="2" x2="8" y2="6"></line>
-                                                        <line x1="3" y1="10" x2="21" y2="10"></line>
-                                                    </svg> ID Number : <?php echo $std->idnumber; ?>
+                                                    ID Number : <?php echo $std->idnumber; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                                        <line x1="16" y1="2" x2="16" y2="6"></line>
-                                                        <line x1="8" y1="2" x2="8" y2="6"></line>
-                                                        <line x1="3" y1="10" x2="21" y2="10"></line>
-                                                    </svg> Phone Number: <?php echo $std->phone; ?>
+                                                    Phone Number: <?php echo $std->phone; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                                        <line x1="16" y1="2" x2="16" y2="6"></line>
-                                                        <line x1="8" y1="2" x2="8" y2="6"></line>
-                                                        <line x1="3" y1="10" x2="21" y2="10"></line>
-                                                    </svg> Gender : <?php echo $std->gender; ?>
+                                                    Gender : <?php echo $std->gender; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                                        <line x1="16" y1="2" x2="16" y2="6"></line>
-                                                        <line x1="8" y1="2" x2="8" y2="6"></line>
-                                                        <line x1="3" y1="10" x2="21" y2="10"></line>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                                        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                                        <circle cx="8.5" cy="7" r="4"></circle>
+                                                        <polyline points="17 11 19 13 23 9"></polyline>
                                                     </svg> DOB : <?php echo $std->dob; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin">
-                                                        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                                        <circle cx="12" cy="10" r="3"></circle>
-                                                    </svg>Address : <?php echo $std->adr; ?>
+                                                    Address : <?php echo $std->adr; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin">
-                                                        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                                        <circle cx="12" cy="10" r="3"></circle>
-                                                    </svg>Country : <?php echo $std->country; ?>
+                                                    Country : <?php echo $std->country; ?>
                                                 </li>
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin">
-                                                        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                                        <circle cx="12" cy="10" r="3"></circle>
-                                                    </svg>Course : <?php echo $std->course_name; ?>
+                                                    Course : <?php echo $std->course_name; ?>
                                                 </li>
-                                                <!-- <li class="contacts-block__item">
-                                                    <a href="mailto:example@mail.com"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
-                                                            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                                                            <polyline points="22,6 12,13 2,6"></polyline>
-                                                        </svg>Jimmy@gmail.com</a>
-                                                </li>
-                                                <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
-                                                        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                                                    </svg> +1 (530) 555-12121
-                                                </li> -->
 
                                             </ul>
                                         </div>
@@ -210,6 +158,9 @@ require_once('partials/_head.php');
                                                         <th>
                                                             <div class="th-content">Academic Yr</div>
                                                         </th>
+                                                        <th>
+                                                            <div class="th-content">Date Enrolled</div>
+                                                        </th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
@@ -231,6 +182,9 @@ require_once('partials/_head.php');
                                                             </td>
                                                             <td>
                                                                 <div class="td-content"><span class="discount-pricing"><?php echo $enrollment->enroll_aca_yr; ?></span></div>
+                                                            </td>
+                                                            <td>
+                                                                <div class="td-content"><span class="discount-pricing"><?php echo date('d-M-Y', strtotime($enrollment->created_at)); ?></span></div>
                                                             </td>
                                                         </tr>
                                                     <?php } ?>

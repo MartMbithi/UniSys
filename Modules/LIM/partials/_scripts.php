9ac79ab2591cb1e7a66fc4e894beac09bae6dcee
MartMbithi <martdevelopers254@gmail.com>
2020-10-12 17:12:57 +0300

    Intergrated Chart Js

--- Modules/LIM/partials/_scripts.php
+++ Modules/LIM/partials/_scripts.php
@@ -15,153 +15,6 @@
     <script>
         try {
 
-            /*
-                ==============================
-                |    @Options Charts Script   |
-                ==============================
-            */
-
-            /*
-                =============================
-                    Daily Sales | Options
-                =============================
-            */
-            var d_2options1 = {
-                chart: {
-                    height: 160,
-                    type: 'bar',
-                    stacked: true,
-                    stackType: '100%',
-                    toolbar: {
-                        show: false,
-                    }
-                },
-                dataLabels: {
-                    enabled: false,
-                },
-                stroke: {
-                    show: true,
-                    width: 1,
-                },
-                colors: ['#e2a03f', '#e0e6ed'],
-                responsive: [{
-                    breakpoint: 480,
-                    options: {
-                        legend: {
-                            position: 'bottom',
-                            offsetX: -10,
-                            offsetY: 0
-                        }
-                    }
-                }],
-                series: [{
-                    name: 'Sales',
-                    data: [44, 55, 41, 67, 22, 43, 21]
-                }, {
-                    name: 'Last Week',
-                    data: [13, 23, 20, 8, 13, 27, 33]
-                }],
-                xaxis: {
-                    labels: {
-                        show: false,
-                    },
-                    categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
-                },
-                yaxis: {
-                    show: false
-                },
-                fill: {
-                    opacity: 1
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        endingShape: 'rounded',
-                        columnWidth: '25%',
-                    }
-                },
-                legend: {
-                    show: false,
-                },
-                grid: {
-                    show: false,
-                    xaxis: {
-                        lines: {
-                            show: false
-                        }
-                    },
-                    padding: {
-                        top: 10,
-                        right: 0,
-                        bottom: -40,
-                        left: 0
-                    },
-                },
-            }
-
-            /*
-                =============================
-                    Total Orders | Options
-                =============================
-            */
-            var d_2options2 = {
-                chart: {
-                    id: 'sparkline1',
-                    group: 'sparklines',
-                    type: 'area',
-                    height: 280,
-                    sparkline: {
-                        enabled: true
-                    },
-                },
-                stroke: {
-                    curve: 'smooth',
-                    width: 2
-                },
-                fill: {
-                    opacity: 1,
-                },
-                series: [{
-                    name: 'Sales',
-                    data: [28, 40, 36, 52, 38, 60, 38, 52, 36, 40]
-                }],
-                labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-                yaxis: {
-                    min: 0
-                },
-                grid: {
-                    padding: {
-                        top: 125,
-                        right: 0,
-                        bottom: 36,
-                        left: 0
-                    },
-                },
-                fill: {
-                    type: "gradient",
-                    gradient: {
-                        type: "vertical",
-                        shadeIntensity: 1,
-                        inverseColors: !1,
-                        opacityFrom: .40,
-                        opacityTo: .05,
-                        stops: [45, 100]
-                    }
-                },
-                tooltip: {
-                    x: {
-                        show: false,
-                    },
-                    theme: 'dark'
-                },
-                colors: ['#fff']
-            }
-
-            /*
-                =================================
-                    Revenue Monthly | Options
-                =================================
-            */
             var options1 = {
                 chart: {
                     fontFamily: 'Nunito, sans-serif',
@@ -382,11 +235,7 @@
                 }]
             }
 
-            /*
-                ==================================
-                    Sales By Category | Options
-                ==================================
-            */
+
             var options = {
                 chart: {
                     type: 'donut',
@@ -486,35 +335,12 @@
                 }]
             }
 
+            var chart = new ApexCharts(
+                document.querySelector("#chart-2"),
+                options
+            );
 
-            /*
-                ==============================
-                |    @Render Charts Script    |
-                ==============================
-            */
-
-
-            /*
-                ============================
-                    Daily Sales | Render
-                ============================
-            */
-            var d_2C_1 = new ApexCharts(document.querySelector("#daily-sales"), d_2options1);
-            d_2C_1.render();
-
-            /*
-                ============================
-                    Total Orders | Render
-                ============================
-            */
-            var d_2C_2 = new ApexCharts(document.querySelector("#total-orders"), d_2options2);
-            d_2C_2.render();
-
-            /*
-                ================================
-                    Revenue Monthly | Render
-                ================================
-            */
+            chart.render();
             var chart1 = new ApexCharts(
                 document.querySelector("#revenueMonthly"),
                 options1
@@ -522,23 +348,7 @@
 
             chart1.render();
 
-            /*
-                =================================
-                    Sales By Category | Render
-                =================================
-            */
-            var chart = new ApexCharts(
-                document.querySelector("#chart-2"),
-                options
-            );
 
-            chart.render();
-
-            /*
-                =============================================
-                    Perfect Scrollbar | Recent Activities
-                =============================================
-            */
             const ps = new PerfectScrollbar(document.querySelector('.mt-container'));
 
 
@@ -549,7 +359,7 @@
 
     <script>
         try {
-/* Monthly Library Users */
+            /* Monthly Library Users */
             var d_1options1 = {
                 chart: {
                     height: 350,

9722569b65d6f55bc4c7b9cdbfc63f185dd6872f
MartMbithi <martdevelopers254@gmail.com>
2020-10-12 10:32:28 +0300

    Auth On UniSysy Library Information Management Module

--- Modules/LIM/partials/_head.php
+++ Modules/LIM/partials/_head.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-  <title>UniSys | Students Records Management Module</title>
+  <title>UniSys | Library Information Management Module</title>
   <!--   <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
  -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">

c6786301d7c969c65006f7e43c31bd0530e9fd35
MartMbithi <martdevelopers254@gmail.com>
2020-10-13 11:50:54 +0300

    Current Date / Time API

--- Modules/LIM/dashboard.php
+++ Modules/LIM/dashboard.php
@@ -85,9 +85,9 @@ require_once('partials/_head.php');
                     <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-chart-one">
                             <div class="widget-heading">
-                                <h5 class="">Monthly Library Usage</h5>
+                                <h5 class="">Monthly Library Operations</h5>
                                 <ul class="tabs tab-pills">
-                                    <li><a href="javascript:void(0);" id="tb_1" class="tabmenu">Monthly</a></li>
+                                    <li><a href="javascript:void(0);" id="tb_1" class="tabmenu"><?php echo date('d M Y');?></a></li>
                                 </ul>
                             </div>
 
@@ -113,7 +113,7 @@ require_once('partials/_head.php');
                         </div>
                     </div>
 
-                    <div class="col-xl-5 col-lg-12 col-md-6 col-sm-12 col-12 layout-spacing">
+                    <div class="col-xl-8 col-lg-12 col-md-6 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-table-one">
                             <div class="widget-heading">
                                 <h5 class="">Library Operations At Glance</h5>
@@ -154,54 +154,6 @@ require_once('partials/_head.php');
                                     </div>
                                 <?php } ?>
 
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
-
-                        <div class="widget widget-activity-four">
-
-                            <div class="widget-heading">
-                                <h5 class="">Library Logs / Activities</h5>
-                                <!-- 
-                                    If its lost book, indicate red
-                                    if its damanged book indicate warning
-                                    if its retured in good condition indicate green
-                                    Sort by latest 
-                                 -->
-                            </div>
-                            <div class="widget-content">
-                                <div class="mt-container mx-auto">
-                                    <div class="timeline-line">
-                                        <div class="item-timeline timeline-primary">
-                                            <div class="t-dot" data-original-title="" title="">
-                                            </div>
-                                            <div class="t-text">
-                                                <p><span>Updated</span> Server Logs</p>
-                                                <span class="badge badge-danger">Pending</span>
-                                                <p class="t-time">Just Now</p>
-                                            </div>
-                                        </div>
-                                        <div class="item-timeline timeline-success">
-                                            <div class="t-dot" data-original-title="" title="">
-                                            </div>
-                                            <div class="t-text">
-                                                <p>Send Mail to <a href="javascript:void(0);">HR</a> and <a href="javascript:void(0);">Admin</a></p>
-                                                <span class="badge badge-success">Completed</span>
-                                                <p class="t-time">2 min ago</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="tm-action-btn">
-                                    <a href="unisys_lim_library_operations.php" class="btn">View All
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
-                                            <polyline points="6 9 12 15 18 9"></polyline>
-                                        </svg>
-                                    </a>
-                                </div>
                             </div>
                         </div>
                     </div>

40f4b9cffa3c406905d9ce173f97982f2cc2bc7a
MartMbithi <martdevelopers254@gmail.com>
2020-10-14 13:06:44 +0300

    Reduce Book Count When Borrowed

--- Modules/LIM/unisys_lim_add_library_operation.php
+++ Modules/LIM/unisys_lim_add_library_operation.php
@@ -163,7 +163,18 @@ require_once('partials/_head.php');
                                             while ($book = $res->fetch_object()) {
                                             ?>
                                                 <option><?php echo $book->isbn; ?></option>
-                                            <?php } ?>
+                                            <?php }
+                                            $bookIsnNumber = $book->isbn;
+                                            $ret = "SELECT * FROM `UniSys_LIM_Books_Cataloque` WHERE isbn = '$bookIsnNumber'  ";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($book = $res->fetch_object()) {
+                                                $initialBookCount = $book->copies;
+                                                $newBookCount = $initialBookCount - 1;
+                                            }
+                                            ?>
+                                            <input type="hidden" class="form-control" name="copies" value="<?php echo $newBookCount; ?>">
                                         </select>
                                     </div>
                                     <div class="form-group col-md-6">

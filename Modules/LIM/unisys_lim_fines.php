b6efc2d5a6534b97f9a36a3bf922e583dc5145c5
MartMbithi <martdevelopers254@gmail.com>
2020-10-15 12:04:26 +0300

    Fines

---
+++ Modules/LIM/unisys_lim_fines.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+require_once('configs/config.php');
+require_once('configs/checklogin.php');
+check_login();
+require_once('partials/_analytics.php');
+
+require_once('partials/_head.php');
+?>
+
+<body>
+
+    <!--  BEGIN NAVBAR  -->
+    <?php require_once('partials/_nav.php'); ?>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN NAVBAR  -->
+    <div class="sub-header-container">
+        <header class="header navbar navbar-expand-sm">
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg></a>
+
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Library Fines</span></li>
+                            </ol>
+                        </nav>
+
+                    </div>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <?php require_once('partials/_sidebar.php'); ?>
+        <!--  END SIDEBAR  -->
+
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                <div class="row layout-top-spacing">
+                    <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+                        <div class="widget-content widget-content-area br-6">
+                            <div class="table-responsive mb-4 mt-4">
+                                <table id="alter_pagination" class="table table-hover" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Checksum</th>
+                                            <th>Type</th>
+                                            <th>Book Isbn</th>
+                                            <th>Book Title</th>
+                                            <th>Student Name</th>
+                                            <th>Student ADMNo</th>
+                                            <th>Created At</th>
+                                            <th class="text-center">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+
+                                        $ret = "SELECT * FROM `UniSys_LIM_Library_Operations` WHERE (type ='Lost ' || type = 'Damanged') AND payment_status ='Pending' ";
+                                        $stmt = $mysqli->prepare($ret);
+                                        $stmt->execute(); //ok
+                                        $res = $stmt->get_result();
+                                        while ($ops = $res->fetch_object()) {
+                                        ?>
+                                            <tr>
+                                                <td><span class="text-success"><?php echo $ops->checksum; ?></span></td>
+                                                <td>
+                                                    <?php
+                                                    if ($ops->type == 'Damanged') {
+                                                        echo "<span class='badge outline-badge-warning'>$ops->type</span>";
+                                                    } elseif ($ops->type == 'Lost') {
+                                                        echo "<span class='badge outline-badge-danger'>$ops->type</span>";
+                                                    } elseif ($ops->type == 'Return') {
+                                                        echo "<span class='badge outline-badge-success'>$ops->type</span>";
+                                                    } else {
+                                                        echo "<span class='badge outline-badge-primary'>$ops->type</span>";
+                                                    }
+                                                    ?>
+                                                </td>
+                                                <td><?php echo $ops->book_isbn; ?></td>
+                                                <td><?php echo $ops->book_title; ?></td>
+                                                <td><?php echo $ops->student_name; ?></td>
+                                                <td><?php echo $ops->student_regno; ?></td>
+                                                <td><?php echo date('d, M, Y g:i', strtotime($ops->created_at)); ?></td>
+                                                <td class="text-center">
+                                                    <a class="badge outline-badge-success" href="unisys_lim_pay_fine.php?pay=<?php echo $ops->id; ?>&status=Paid">Pay Fine</a>
+                                                </td>
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <?php require_once('partials/_footer.php'); ?>
+        </div>
+        <!--  END CONTENT AREA  -->
+    </div>
+    <!-- END MAIN CONTAINER -->
+
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>

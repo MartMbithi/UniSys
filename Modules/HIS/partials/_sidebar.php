42c77582a1371cacf7b92159d28eae41b13dc200
MartMbithi <martdevelopers254@gmail.com>
2020-10-16 12:25:41 +0300

    Minor Update On Side Navigation

--- Modules/HIS/partials/_sidebar.php
+++ Modules/HIS/partials/_sidebar.php
@@ -35,7 +35,19 @@
                             <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
                             <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
                         </svg>
-                        <span>Books Cataloque</span>
+                        <span>Hostels</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="menu">
+                <a href="unisys_lim_books_cataloque.php" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+                            <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+                        </svg>
+                        <span>Hostel Rooms</span>
                     </div>
                 </a>
             </li>
@@ -49,7 +61,7 @@
                             <polyline points="11 6 7 12 13 12 9 18"></polyline>
                         </svg>
 
-                        <span>BarCodes</span>
+                        <span>Room Allocations</span>
                     </div>
                 </a>
             </li>
@@ -60,52 +72,11 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
                             <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                         </svg>
-                        <span>Library Operations</span>
+                        <span>Hostel Inventory</span>
                     </div>
                 </a>
             </li>
 
-            <li class="menu">
-                <a href="unisys_lim_fines.php" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                            <circle cx="12" cy="8" r="7"></circle>
-                            <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
-                        </svg>
-                        <span>Fines</span>
-                    </div>
-                </a>
-            </li>
-
-            <li class="menu">
-                <a href="unisys_lim_library_register.php" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                            <polyline points="14 2 14 8 20 8"></polyline>
-                            <line x1="16" y1="13" x2="8" y2="13"></line>
-                            <line x1="16" y1="17" x2="8" y2="17"></line>
-                            <polyline points="10 9 9 9 8 9"></polyline>
-                        </svg>
-                        <span>Lib Register</span>
-                    </div>
-                </a>
-            </li>
-
-            <!-- <li class="menu">
-                <a href="unisys_lim_library_register.php" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                            <polyline points="14 2 14 8 20 8"></polyline>
-                            <line x1="16" y1="13" x2="8" y2="13"></line>
-                            <line x1="16" y1="17" x2="8" y2="17"></line>
-                            <polyline points="10 9 9 9 8 9"></polyline>
-                        </svg>
-                        <span>OPAC</span>
-                    </div>
-                </a>
-            </li> -->
 
             <li class="menu">
                 <a href="#reports" data-active="false" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
@@ -125,16 +96,16 @@
                 </a>
                 <ul class="collapse submenu list-unstyled" id="reports" data-parent="#accordionExample">
                     <li class="active">
-                        <a href="unisys_lim_reports_books_inventory.php"> Books Inventory </a>
+                        <a href="unisys_lim_reports_books_inventory.php"> Hostels </a>
                     </li>
                     <li>
-                        <a href="unisys_lim_reports_library_register.php"> Register</a>
+                        <a href="unisys_lim_reports_library_register.php"> Rooms</a>
                     </li>
                     <li>
-                        <a href="unisys_lim_reports_library_operations.php"> Operations </a>
+                        <a href="unisys_lim_reports_library_operations.php"> Allocations </a>
                     </li>
                     <li>
-                        <a href="unisys_lim_reports_fines.php"> Fines </a>
+                        <a href="unisys_lim_reports_fines.php"> Assets </a>
                     </li>
                 </ul>
             </li>

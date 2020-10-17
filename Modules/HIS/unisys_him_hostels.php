4f158e989508645f658878351d27654d6d3e6778
MartMbithi <martdevelopers254@gmail.com>
2020-10-17 19:37:06 +0300

    Create Hostel Backend

--- Modules/HIS/unisys_him_hostels.php
+++ Modules/HIS/unisys_him_hostels.php
@@ -73,7 +73,7 @@ require_once('partials/_head.php');
                 <div class="row layout-top-spacing">
                     <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
                         <div class="widget-content widget-content-area br-6">
-                            <a class="btn btn-outline-success" href="unisys_lim_add_book.php">
+                            <a class="btn btn-outline-success" href="unisys_him_add_hostel.php">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
                                     <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
                                     <polyline points="14 2 14 8 20 8"></polyline>

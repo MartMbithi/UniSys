5912249e992b777438ea70401eeb3b8aebc85f78
MartMbithi <martdevelopers254@gmail.com>
2020-11-27 19:03:16 +0300

    Analytics On Jobs Posted

--- Modules/HRM/partials/_analytics.php
+++ Modules/HRM/partials/_analytics.php
@@ -1,9 +1,21 @@
 <?PHP
 /* Staffs */
-$staffs = 0;
+$query = "SELECT COUNT(*)  FROM `UniSys_Staffs` ";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($staffs);
+$stmt->fetch();
+$stmt->close();
+
 /* Jobs Posted*/
-$jobs_posted = 0;
+$query = "SELECT COUNT(*)  FROM `UniSys_JobsPosted` ";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($jobs_posted);
+$stmt->fetch();
+$stmt->close();
 /* Unpaid Benefits */
+
 $unpaid_benefits = 0;
 /* Paid Benefits */
 $paid_benefits = 0;

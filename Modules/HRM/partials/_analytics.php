a4d6fab5159b3c9f71e12f35b3d900d064072d15
MartMbithi <martdevelopers254@gmail.com>
2020-11-27 18:54:58 +0300

    Analytics Updated

--- Modules/HRM/partials/_analytics.php
+++ Modules/HRM/partials/_analytics.php
@@ -1,7 +1,13 @@
 <?PHP
 /* Staffs */
+$staffs = 0;
 /* Jobs Posted*/
+$jobs_posted = 0;
 /* Unpaid Benefits */
+$unpaid_benefits = 0;
 /* Paid Benefits */
+$paid_benefits = 0;
 /* Pending Payrolls */
+$pending_payrolls = 0;
 /* Paid Payrolls */
+$paid_payrolls = 0 ;

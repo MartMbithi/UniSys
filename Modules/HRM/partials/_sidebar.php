42be5d9ab41015ca8e0d5deb17b8393f9d4656a3
MartMbithi <martdevelopers254@gmail.com>
2020-11-27 18:56:02 +0300

    Updated Sidebar Icons

--- Modules/HRM/partials/_sidebar.php
+++ Modules/HRM/partials/_sidebar.php
@@ -1,14 +1,13 @@
 <div class="sidebar-wrapper sidebar-theme">
-
     <nav id="sidebar">
         <div class="shadow-bottom"></div>
         <ul class="list-unstyled menu-categories" id="accordionExample">
             <li class="menu">
                 <a href="#dashboard" data-active="false" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
-                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                            <polyline points="13 17 18 12 13 7"></polyline>
+                            <polyline points="6 17 11 12 6 7"></polyline>
                         </svg>
                         <span>Dashboard</span>
                     </div>
@@ -164,9 +163,8 @@
                 <a href="logout.php" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                            <path d="M10 22H5a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h5"></path>
-                            <polyline points="17 16 21 12 17 8"></polyline>
-                            <line x1="21" y1="12" x2="9" y2="12"></line>
+                            <polyline points="13 17 18 12 13 7"></polyline>
+                            <polyline points="6 17 11 12 6 7"></polyline>
                         </svg>
                         <span>End Session</span>
                     </div>
